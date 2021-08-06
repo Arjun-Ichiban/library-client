a25e77bcd73df278982127378e1bfcef5f402900
Arjun S <83405143+Arjun-Ichiban@users.noreply.github.com>
2021-08-06 12:08:27 +0530

    Add ShowBookDetails.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Route exact path='/' component={ShowBookList} />
           <Route path='/create-book' component={CreateBook} />
         </div>
       </Router>
